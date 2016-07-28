7f99e3587ae2e156cb5ff41c71281815a8b42f52
rzouak <rihab.challenge@gmail.com>
2016-07-28 18:00:48 +0100

    Update Test.java
    ajout des attributs

--- NewProjectGit/src/Test.java
+++ NewProjectGit/src/Test.java
@@ -8,6 +8,8 @@ public class Test {
 	  private String pays;
 	  private String nom;
 	  private String prenom;
+	    private String nom1;
+	  private String prenom2;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
