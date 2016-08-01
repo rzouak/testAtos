108985d63c6276669583beefa8a0421afcded21e
rzouak <rihab.challenge@gmail.com>
2016-08-01 10:54:20 +0100

    Update Atos.java

--- NewProjectGit/src/Atos.java
+++ NewProjectGit/src/Atos.java
@@ -4,5 +4,13 @@ public class Atos {
 	  private String adresse;
 	  private String tel;
 	  private String capital;
+	   private String nomination1;
+	  private String adresse1;
+	  private String tel1;
+	  private String capital1;
+	   private String nomination2;
+	  private String adresse2;
+	  private String tel2;
+	  private String capital2;
 
 }
